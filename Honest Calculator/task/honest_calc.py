de4bf7678436cff067ba233d0e9f19124f8f4cf1
Roman92P <roman.paszkow@gmail.com>
2023-02-03 09:18:46 +0100

    new commit

--- Honest Calculator/task/honest_calc.py
+++ Honest Calculator/task/honest_calc.py
@@ -4,9 +4,11 @@ msg_1 = "Do you even know what numbers are? Stay focused!"
 
 msg_2 = "Yes ... an interesting math operation. You've slept through all classes, haven't you?"
 
+msg_3 = "Yeah... division by zero. Smart move..."
 opers = ['+', '-', '*', '/']
 
 while True:
+    result = 0
     read_calc = str(input(msg_0))
     read_calc_arr = read_calc.split(' ')
     x = read_calc_arr[0]
@@ -19,4 +21,23 @@ while True:
     elif not opers.__contains__(oper):
         print(msg_2)
         continue
+
+    x = float(x)
+    y = float(y)
+
+    if oper == '+':
+        result = x + y
+        print(result)
+    if oper == '-':
+        result = x - y
+        print(result)
+    if oper == '*':
+        result = x * y
+        print(result)
+    if oper == '/':
+        if y != '0':
+            print(msg_3)
+            continue
+        result = x / y
+        print(result)
     break
