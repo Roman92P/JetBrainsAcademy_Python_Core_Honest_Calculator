d0bb0c159d4999e5189ebdd41b915b24e61ad534
Roman92P <roman.paszkow@gmail.com>
2023-02-12 12:07:16 +0100

    3 stage is complete

--- Honest Calculator/task/honest_calc.py
+++ Honest Calculator/task/honest_calc.py
@@ -4,14 +4,33 @@ msg_1 = "Do you even know what numbers are? Stay focused!"
 
 msg_2 = "Yes ... an interesting math operation. You've slept through all classes, haven't you?"
 
+msg_3 = "Yeah... division by zero. Smart move..."
+
+msg_4 = "Do you want to store the result? (y / n):"
+
+msg_5 = "Do you want to continue calculations? (y / n):"
+
 opers = ['+', '-', '*', '/']
 
-while True:
+memory = 0
+
+continue_calculation = True
+
+while continue_calculation:
+
+    result = 0
     read_calc = str(input(msg_0))
     read_calc_arr = read_calc.split(' ')
     x = read_calc_arr[0]
     oper = read_calc_arr[1]
     y = read_calc_arr[2]
+
+    if x == 'M':
+        x = str(memory)
+
+    if y == 'M':
+        y = str(memory)
+
     if not x.replace('.', '').isnumeric() or not y.replace('.', '').isnumeric():
         print(msg_1)
         continue
@@ -19,4 +38,35 @@ while True:
     elif not opers.__contains__(oper):
         print(msg_2)
         continue
-    break
+
+    x = float(x)
+    y = float(y)
+
+    if oper == '+':
+        result = x + y
+        print(result)
+    if oper == '-':
+        result = x - y
+        print(result)
+    if oper == '*':
+        result = x * y
+        print(result)
+    if oper == '/':
+        if y != '0':
+            print(msg_3)
+            continue
+        result = x / y
+        print(result)
+
+    answer = ''
+    while answer != 'y' and answer != 'n':
+        answer = str(input(msg_4))
+        if answer == 'y':
+            memory = result
+
+    new_calc = ''
+    while new_calc != 'y' and new_calc != 'n':
+        new_calc = str(input(msg_5))
+        if new_calc == 'n':
+            continue_calculation = False
+
--- Honest Calculator/task/test/tests.py
+++ Honest Calculator/task/test/tests.py
@@ -2,14 +2,27 @@ from hstest import StageTest, CheckResult, WrongAnswer, dynamic_test, TestedProg
 
 msg = ["Enter an equation",
        "Do you even know what numbers are? Stay focused!",
-       "Yes ... an interesting math operation. You've slept through all classes, haven't you?"]
+       "Yes ... an interesting math operation. You've slept through all classes, haven't you?",
+       "Yeah... division by zero. Smart move...",
+       "Do you want to store the result? (y / n):",
+       "Do you want to continue calculations? (y / n):"]
+
+
+def add_enter(txt):
+    return "\n".join([txt, msg[0]])
+
+
+def add_memory(txt):
+    return "\n".join([txt, msg[4]])
+
 
 data = [
-            (("2 + 1.1", ""), ),
-            (("2 + m", "\n".join([msg[1], msg[0]])), ("3 + 3", "")),
-            (("2 + m", "\n".join([msg[1], msg[0]])), ("3 n 3", "\n".join([msg[2], msg[0]])),
-             ("m - 2", "\n".join([msg[1], msg[0]])), ("4 * 5.2", "")),
-
+            (("4 * 5", add_memory("20.0")), ("y", msg[5]), ("n", "")),
+            (("4 * 5.2", add_memory("20.8")), ("y", msg[5]), ("y", msg[0]),
+             ("1 + M", add_memory("21.8")), ("y", msg[5]), ("n", "")),
+            (("2 + 5", add_memory("7.0")), ("n", msg[5]), ("y", msg[0]),
+             ("21.0 / M", add_enter(msg[3])), ("5 + M", add_memory("5.0")),
+             ("y", msg[5]), ("n", "")),
        ]  # (input data, msg sentence])
 
 
@@ -26,6 +39,7 @@ class HonestCalc(StageTest):
                 return CheckResult.wrong(f"Expected: ({item[1]});\nFound:    ({output.strip()})")
         if not pr.is_finished():
             return CheckResult.wrong("Your program unnecessarily waiting for input.")
+
         return CheckResult.correct()
 
 
