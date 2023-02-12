8b47ea6aabd6c561d96ceeb08a8dd6196638a2ad
Roman92P <roman.paszkow@gmail.com>
2023-02-12 13:07:18 +0100

    stage 4 complete

--- Honest Calculator/task/honest_calc.py
+++ Honest Calculator/task/honest_calc.py
@@ -10,11 +10,37 @@ msg_4 = "Do you want to store the result? (y / n):"
 
 msg_5 = "Do you want to continue calculations? (y / n):"
 
+msg_6 = " ... lazy"
+
+msg_7 = " ... very lazy"
+
+msg_8 = " ... very, very lazy"
+
+msg_9 = "You are"
+
 opers = ['+', '-', '*', '/']
 
 memory = 0
 
 continue_calculation = True
+
+
+def is_one_digit(v):
+    return -10 < v < 10 and v.is_integer()
+
+
+def check(v1, v2, v3):
+    msg = ""
+    if is_one_digit(v1) and is_one_digit(v2):
+        msg += msg_6
+    if (v1 == 1 or v2 == 1) and v3 == '*':
+        msg += msg_7
+    if (v1 == 0 or v2 == 0) and (v3 == '*' or v3 == '+' or v3 == '-'):
+        msg += msg_8
+    if msg != '':
+        msg = msg_9 + msg
+        print(msg)
+
 
 while continue_calculation:
 
@@ -41,6 +67,8 @@ while continue_calculation:
 
     x = float(x)
     y = float(y)
+
+    check(x, y, oper)
 
     if oper == '+':
         result = x + y
@@ -69,4 +97,3 @@ while continue_calculation:
         new_calc = str(input(msg_5))
         if new_calc == 'n':
             continue_calculation = False
-
